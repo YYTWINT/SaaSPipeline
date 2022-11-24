5bbef69092d0d9abb94c1098e44b1758e6b39d2c
YYTWINT <yytwint.sisw@siemens.com>
2022-11-24 14:49:55 +0530

    Update createTranslatorWorkerUnittest.bat

--- Scripts/wntx64/createTranslatorWorkerUnittest.bat
+++ Scripts/wntx64/createTranslatorWorkerUnittest.bat
@@ -1,21 +1,22 @@
 @echo off
 
-SET NX_RELEASE=%1
-SET UNIT_PATH=%2
-SET HC_FLAG=%3
-SET UNIT_BAT="C:\\apps\\devop_tools\\UDU\\tools\\bin\\wnt\\unit.bat"
+echo "hi Jenkins"
+REM SET NX_RELEASE=%1
+REM SET UNIT_PATH=%2
+REM SET HC_FLAG=%3
+REM SET UNIT_BAT="C:\\apps\\devop_tools\\UDU\\tools\\bin\\wnt\\unit.bat"
 
 
-if %HC_FLAG%==false (
-	call %UNIT_BAT% add -b -p @%NX_RELEASE% -t DEV -w SUB %UNIT_PATH% -R y -O y -DO_LINK_OPT y
-) else (
-	call %UNIT_BAT% add -b -p @%NX_RELEASE% -t DEV -w SUB %UNIT_PATH% -R y -O y -DO_LINK_OPT y
+REM if %HC_FLAG%==false (
+	REM call %UNIT_BAT% add -b -p @%NX_RELEASE% -t DEV -w SUB %UNIT_PATH% -R y -O y -DO_LINK_OPT y
+REM ) else (
+	REM call %UNIT_BAT% add -b -p @%NX_RELEASE% -t DEV -w SUB %UNIT_PATH% -R y -O y -DO_LINK_OPT y
 
-	REM SET initFile=%UNIT_PATH%/init.def
-	REM sed -i 's/DO_TARI_RECOMPILES.*/DO_TARI_RECOMPILES       1/g' %initFile%
-	REM sed -i 's/DO_SOURCE_RECOMPILES.*/DO_SOURCE_RECOMPILES       1/g' %initFile%
-	REM sed -i 's/DO_LINK_OPT.*/DO_LINK_OPT       1/g' %initFile%
-	REM sed -i 's/DO_DEBUG.*/DO_DEBUG       0/g' %initFile%
-	REM sed -i 's/DO_QAZ.*/DO_QAZ       1/g' %initFile%
-	REM sed -i 's/DO_DLL_COMPARISON.*/DO_DLL_COMPARISON       1/g' %initFile%
-) 
+	REM REM SET initFile=%UNIT_PATH%/init.def
+	REM REM sed -i 's/DO_TARI_RECOMPILES.*/DO_TARI_RECOMPILES       1/g' %initFile%
+	REM REM sed -i 's/DO_SOURCE_RECOMPILES.*/DO_SOURCE_RECOMPILES       1/g' %initFile%
+	REM REM sed -i 's/DO_LINK_OPT.*/DO_LINK_OPT       1/g' %initFile%
+	REM REM sed -i 's/DO_DEBUG.*/DO_DEBUG       0/g' %initFile%
+	REM REM sed -i 's/DO_QAZ.*/DO_QAZ       1/g' %initFile%
+	REM REM sed -i 's/DO_DLL_COMPARISON.*/DO_DLL_COMPARISON       1/g' %initFile%
+REM ) 
