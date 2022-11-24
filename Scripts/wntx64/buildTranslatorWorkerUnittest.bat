121d51b7d873fa901216583246b3569dfd050d99
YYTWINT <yytwint.sisw@siemens.com>
2022-11-24 14:23:42 +0530

    updated

--- Scripts/wntx64/buildTranslatorWorkerUnittest.bat
+++ Scripts/wntx64/buildTranslatorWorkerUnittest.bat
@@ -4,14 +4,11 @@ SET UNIT_PATH=%1
 SET CP_NUMBER=%2
 SET HC_FLAG=%3
 SET UNIT_BAT="C:\apps\devop_tools\UDU\tools\bin\wnt\unit.bat"
-if "%HC_FLAG%" equ "false" 
-(
+if %HC_FLAG%==false (
   call %UNIT_BAT% run %UNIT_PATH% b product TranslatorWorker
   call %UNIT_BAT% run %UNIT_PATH% b product validate_worker TranslatorWorker
   echo "here in windows hc false"
-)
-else
-(
+) else (
   call %UNIT_BAT% run %UNIT_PATH% C:\\apps\\devop_tools\\bin\\dtcmd.pl cli set -C -A %CP_NUMBER%
   call %UNIT_BAT% run %UNIT_PATH% b
   call %UNIT_BAT% run %UNIT_PATH% b image ugtopv
--- Scripts/wntx64/createTranslatorWorkerUnittest.bat
+++ Scripts/wntx64/createTranslatorWorkerUnittest.bat
@@ -2,15 +2,17 @@
 
 SET NX_RELEASE=%1
 SET UNIT_PATH=%2
-SET HC_FLAG=%3
+SET HC_FLAG= %3
 SET UNIT_BAT="C:\apps\devop_tools\UDU\tools\bin\wnt\unit.bat"
-if "%HC_FLAG%" equ "false" 
-(
-  call %UNIT_BAT% add -b -p @%NX_RELEASE% -t DEV -w SUB %UNIT_PATH% -R y -O y -DO_LINK_OPT y
-  echo "%UNIT_BAT% add -b -p @%NX_RELEASE% -t DEV -w SUB %UNIT_PATH% -R y -O y -DO_LINK_OPT y"
-)
-else
-(
+echo %NX_RELEASE%
+echo %UNIT_BAT%
+echo %UNIT_PATH%
+echo %HC_FLAG%
+
+if %HC_FLAG%==false (
+	echo %HC_FLAG% 
+	call %UNIT_BAT% add -b -p @%NX_RELEASE% -t DEV -w SUB %UNIT_PATH% -R y -O y -DO_LINK_OPT y
+) else (
 	call %UNIT_BAT% add -b -p @%NX_RELEASE% -t DEV -w SUB %UNIT_PATH% -R y -O y -DO_LINK_OPT y
 
 	REM SET initFile=%UNIT_PATH%/init.def
@@ -20,4 +22,4 @@ else
 	REM sed -i 's/DO_DEBUG.*/DO_DEBUG       0/g' %initFile%
 	REM sed -i 's/DO_QAZ.*/DO_QAZ       1/g' %initFile%
 	REM sed -i 's/DO_DLL_COMPARISON.*/DO_DLL_COMPARISON       1/g' %initFile%
-)
+) 
