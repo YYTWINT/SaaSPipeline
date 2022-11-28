64a1ada46c8bf8740c94e6b6877208292b2a7f54
YYTWINT <yytwint.sisw@siemens.com>
2022-11-28 11:42:28 +0530

    updating files

--- Scripts/wntx64/buildTranslatorWorkerUnittest.bat
+++ Scripts/wntx64/buildTranslatorWorkerUnittest.bat
@@ -7,12 +7,12 @@ SET UNIT_BAT="C:\apps\devop_tools\UDU\tools\bin\wnt\unit.bat"
 if %HC_FLAG%==false (
   call %UNIT_BAT% run %UNIT_PATH% b product TranslatorWorker
   call %UNIT_BAT% run %UNIT_PATH% b product validate_worker TranslatorWorker
-  echo "here in windows hc false"
+  
 ) else (
   call %UNIT_BAT% run %UNIT_PATH% C:\\apps\\devop_tools\\bin\\dtcmd.pl cli set -C -A %CP_NUMBER%
   call %UNIT_BAT% run %UNIT_PATH% b
   call %UNIT_BAT% run %UNIT_PATH% b image ugtopv
   call %UNIT_BAT% run %UNIT_PATH% b product TranslatorWorker
   call %UNIT_BAT% run %UNIT_PATH% b product validate_worker TranslatorWorker
-  echo "here in windows hc true"
+  
 )
--- Scripts/wntx64/executeTranslatorWorkerTest.bat
+++ Scripts/wntx64/executeTranslatorWorkerTest.bat
@@ -1,26 +1,20 @@
-#!/bin/bash
+@echo off
 
-if [ $# -lt 1 ] 
-then
-	echo "executeTranslatorWorkerTest.sh called with incorrect number of arguments."
-	echo "executeTranslatorWorkerTest.sh <UnitPath>"
-	echo "For example; executeTranslatorWorkerTest.sh /plm/pnnas/ppic/users/<unit_name>"
-	exit 1
-fi
+rem "executeTranslatorWorkerTest.sh called with incorrect number of arguments."
+rem "executeTranslatorWorkerTest.sh <UnitPath>"
+rem "For example; executeTranslatorWorkerTest.sh /plm/pnnas/ppic/users/<unit_name>"
 
-UNIT_PATH=$1
-export SPLM_LICENSE_SERVER=29000@pnnxflex3:29000@pnnxflex4 
-/usr/site/devop_tools/bin/unit run ${UNIT_PATH} devtest runtest NXTranslators.rep:TranslatorWorker.set -p 6
-casesFailed=`grep "Number of tests:" ${UNIT_PATH}/dt/runs/devtestLastRunFails.txt | cut -d ":" -f2 | tr -d " "`
-if [ $casesFailed != 0 ]
-then
-	/usr/site/devop_tools/bin/unit run ${UNIT_PATH} devtest runfails -p 6 -keep -local -parent
-	casesFailed=`grep "Number of tests:" ${UNIT_PATH}/dt/runs/devtestLastRunFails.txt | cut -d ":" -f2 | tr -d " "`
-	if [ $casesFailed != 0 ]
-	then
+SET UNIT_PATH=%1
+SET UNIT_BAT="C:\apps\devop_tools\UDU\tools\bin\wnt\unit.bat"
+call %UNIT_BAT% run %UNIT_PATH% devtest runtest NXTranslators.rep:TranslatorWorker.set -p 6
+for /F "tokens=2 delims=:" %%a in ('findstr /I "Number of tests:" "%UNIT_PATH%\dt\runs\devtestLastRunFails.txt"') do set "casesFailed=%%a"  
+if %casesFailed% NEQ 0 (
+	call %UNIT_BAT% run %UNIT_PATH% devtest runfails -p 6 -keep -local -parent
+	for /F "tokens=2 delims=:" %%a in ('findstr /I "Number of tests:" "%UNIT_PATH%\dt\runs\devtestLastRunFails.txt"') do set "casesFailed=%%a"  
+	if %casesFailed% NEQ 0 (
 		echo "Number of auto test cases failed = $casesFailed...Exiting with error."
 		exit 1
-	fi
-else
-	echo "No test case failed. So skipping re-run of devtest."
-fi
+	)
+) else (
+	 echo "No test case failed. So skipping re-run of devtest."
+)
