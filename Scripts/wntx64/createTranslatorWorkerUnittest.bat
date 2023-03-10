aad14d785a05c8f9b2d2543673d21a44e1b94406
YYTWINT <yytwint.sisw@siemens.com>
2023-03-10 15:47:20 +0530

    Update createTranslatorWorkerUnittest.bat

--- Scripts/wntx64/createTranslatorWorkerUnittest.bat
+++ Scripts/wntx64/createTranslatorWorkerUnittest.bat
@@ -10,32 +10,32 @@ if %HC_FLAG%==false (
 	call %UNIT_BAT% add -b -p @%NX_RELEASE% -t DEV -w SUB %UNIT_PATH% -R y -O y -DO_LINK_OPT y
 ) else (
 	call %UNIT_BAT% add -b -p @%NX_RELEASE% -t DEV -w SUB %UNIT_PATH% -R y -O y -DO_LINK_OPT y
-	REM SET "ONE=1"
-	REM SET "ZERO=0"
-	REM SET initFile=%UNIT_PATH%/init.def
+	SET "ONE=1"
+	SET "ZERO=0"
+	SET initFile=%UNIT_PATH%/init.def
 
-	REM ( for /f "tokens=1,2* delims=: " %%A in (
-		REM 'findstr /N "^" %initFile%'
-	  REM )  do (
-		REM if "%%B"=="DO_TARI_RECOMPILES" (
-			REM echo=%%B	%ONE%
-		REM ) else if "%%B"=="DO_SOURCE_RECOMPILES" (
-			REM echo=%%B	%ONE%
-		REM )else if "%%B"=="DO_LINK_OPT" (
-			REM echo=%%B	%ONE%
-		REM )else if "%%B"=="DO_QAZ" (
-			REM echo=%%B	%ONE%
-		REM )else if "%%B"=="DO_DLL_COMPARISON" (
-			REM echo=%%B	%ONE%
-		REM )else if "%%B"=="DO_DEBUG" (
-			REM echo=%%B	%ZERO%
-		REM ) else (
-			REM echo=%%B	%%C
-		REM )
-	REM )
-	REM ) >%UNIT_PATH%/init_bk.def
-	REM del %initFile%
-	REM rename %UNIT_PATH%/init_bk.def "init.def"
+	( for /f "tokens=1,2* delims=: " %%A in (
+		'findstr /N "^" %initFile%'
+	  )  do (
+		if "%%B"=="DO_TARI_RECOMPILES" (
+			echo=%%B	%ONE%
+		) else if "%%B"=="DO_SOURCE_RECOMPILES" (
+			echo=%%B	%ONE%
+		)else if "%%B"=="DO_LINK_OPT" (
+			echo=%%B	%ONE%
+		)else if "%%B"=="DO_QAZ" (
+			echo=%%B	%ONE%
+		)else if "%%B"=="DO_DLL_COMPARISON" (
+			echo=%%B	%ONE%
+		)else if "%%B"=="DO_DEBUG" (
+			echo=%%B	%ZERO%
+		) else (
+			echo=%%B	%%C
+		)
+	)
+	) >%UNIT_PATH%/init_bk.def
+	del %initFile%
+	rename %UNIT_PATH%/init_bk.def "init.def"
 		REM SET initFile=%UNIT_PATH%/init.def
 	REM sed -i 's/DO_TARI_RECOMPILES.*/DO_TARI_RECOMPILES       1/g' %initFile%
 	REM sed -i 's/DO_SOURCE_RECOMPILES.*/DO_SOURCE_RECOMPILES       1/g' %initFile%
