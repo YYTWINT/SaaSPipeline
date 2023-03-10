ef3639d609b85ae43bada1d31e1849df4e5cfb28
YYTWINT <yytwint.sisw@siemens.com>
2023-03-10 18:17:10 +0530

    Update createTranslatorWorkerUnittest.bat

--- Scripts/wntx64/createTranslatorWorkerUnittest.bat
+++ Scripts/wntx64/createTranslatorWorkerUnittest.bat
@@ -16,7 +16,7 @@ if %HC_FLAG%==false (
 	SET "ONE=1"
 	SET "ZERO=0"
 	SET initFile=%WIN_PATH%\init.def
-	xcopy /s /i /Y "%UNIT_BAT%/init.def" "%UNIT_BAT%/init_bk.def"
+	xcopy /s /i /Y "%UNIT_PATH%/init.def" "%UNIT_PATH%/init_bk.def"
 	
 	( for /f "tokens=1,2* delims=: " %%A in (
 		'findstr /N "^" %initFile%'
@@ -37,7 +37,7 @@ if %HC_FLAG%==false (
 			echo=%%B	%%C
 		)
 	)
-	)>"%WIN_PATH%\init_bk.def"
+	)>"%UNIT_PATH%/init_bk.def"
 	echo "i am here2"
 	del %initFile%
 	rename %WIN_PATH%\init_bk.def "init.def"
